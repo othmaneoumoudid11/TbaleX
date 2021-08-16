32ccbeb17dd9d88de82fbfa03cd70f66011c0ed4
othmane oumoudid <othmane.oumoudid@um5r.ac.ma>
2021-08-16 20:14:12 +0100

    DataTable1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+
+declare const $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
+  
+
   title = 'Datatable';
+
+  @ViewChild('dTable', {static: false}) dataTable: any;
+
+  ngAfterViewInit(): void {
+    $(this.dataTable.nativeElement).DataTable();
+  }
+
+  ngOnInit(): void {
+
+  }
+
 }
